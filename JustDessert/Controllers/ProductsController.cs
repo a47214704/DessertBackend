aebf9aca60029d3355c0a1997172364fffd54c95
cheng <>
2024-07-10 16:50:57 +0800

    create porject

---
+++ JustDessert/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustDessert.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<ActionResult> Get()
+		{
+			return this.Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Post()
+		{
+			return this.Ok();
+		}
+
+		[HttpPut]
+		public async Task<ActionResult> Put()
+		{
+			return this.Ok();
+		}
+
+		[HttpDelete]
+		public async Task<ActionResult> Delete()
+		{
+			return this.Ok();
+		}
+	}
+}
---
+++ JustDessert/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JustDessert.Models
+{
+	public class Product
+	{
+		[Column("id")]
+		public long Id { get; set; }
+
+		[Column("name")]
+		public string Name { get; set; }
+
+		[Column("description")]
+		public string Description { get; set; }
+
+		[Column("status")]
+		public string Status { get; set; }
+
+		[Column("inventory")]
+		public long Inventory {  get; set; }
+	}
+}
